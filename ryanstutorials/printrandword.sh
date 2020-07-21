42331681aff3ec98b195c6630e4bb2e388fce846
William Goins <willie5216@yahoo.com>
2020-07-21 00:39:13 -0700

    Added filtration of possessives and ability to pass argument to limit word length

--- ryanstutorials/printrandword.sh
+++ ryanstutorials/printrandword.sh
@@ -3,20 +3,32 @@
 ifcontinue=true
 
 while $ifcontinue; do
-	numwords=$(wc -l /usr/share/dict/words)
+	dictloc=/usr/share/dict/words
+
+	dictcontents=$(grep -v "'" $dictloc)
+	#echo -E Dictionary Contents after Grep: $dictcontents
+
+	if [ -z "$1" ]; then
+		echo No arguments supplied
+	else
+		#echo Word length: $1
+		dictcontents=$(grep "^[A-Za-z]\{$1\}$" <<< $dictcontents)
+		#echo Dict Contents Limit String Size: $dictcontents
+	fi
+
+	numwords=$(wc -l <<< $dictcontents)
 	numwords=$(echo $numwords | cut -d ' ' -f1)
 	#echo $numwords
 	
 	randnum=$(shuf -i 0-$numwords -n 1)
-	#echo $randnum
+	echo $randnum
 
-	dictloc=/usr/share/dict/words
-
-	randword=$(sed -n "$randnum"p $dictloc)
+	#randword=$(sed -n "$randnum"p $dictloc)
+	randword=$(sed -n "$randnum"p <<< $dictcontents)
 	echo $randword
 
 	# -n nchars return only after reading exactly NCHAR characters
-        #    unless EOF is encountered or read times out, ignoring
+	#    unless EOF is encountered or read times out, ignoring
 	#    any delimiter
 	# -r do not allow backslashes to escape any characters
 	# -s do not echo input coming from a terminal
