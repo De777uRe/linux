4b2fd84052c9cdd9287dbe04277bf91b8d5b0997
William Goins <willie5216@yahoo.com>
2020-08-09 15:18:18 -0500

    Added script to export configuration files to git repo

---
+++ scripts/exportconfig.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/bash
+
+if [ $# -ne 1 ]; then
+  echo "Please specify location of configuration repo"
+else
+  repo_path=$1
+  
+  echo Copying ~/.bash_aliases ...
+  cp ~/.bash_aliases $1
+  
+  echo Copying ~/.functions ...
+  cp ~/.functions $1
+
+  echo Copying ~/.bashrc ...
+  cp ~/.bashrc $1
+
+  echo Copying .vimrc ...
+  cp ~/.vimrc $1
+fi
